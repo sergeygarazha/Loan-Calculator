5db62b5f845e295c7bb02c3830224dd31527b9bd
Sergey Garazha <garazha.sergey@gmail.com>
2025-12-07 02:39:20 +0300

    add missing imports

--- composeApp/src/commonMain/kotlin/com/example/mkmpa/loan/LoanPreferences.kt
+++ composeApp/src/commonMain/kotlin/com/example/mkmpa/loan/LoanPreferences.kt
@@ -1,5 +1,7 @@
 package com.example.mkmpa.loan
 
+import androidx.compose.runtime.Composable
+
 interface LoanPreferences {
     suspend fun save(amount: Int, periodDays: Int)
     suspend fun load(): SavedLoan?
@@ -7,6 +9,7 @@ interface LoanPreferences {
 
 data class SavedLoan(val amount: Int, val periodDays: Int)
 
+@Composable
 expect fun rememberLoanPreferences(): LoanPreferences
 
 
--- composeApp/src/commonMain/kotlin/com/example/mkmpa/loan/LoanRepository.kt
+++ composeApp/src/commonMain/kotlin/com/example/mkmpa/loan/LoanRepository.kt
@@ -3,6 +3,7 @@ package com.example.mkmpa.loan
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
